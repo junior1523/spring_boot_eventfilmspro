770058361a41630cb5480ed63f1265451da7b361
whoami <peraltaarrietajuan123@gmail.com>
2026-05-12 16:43:22 -0500

    Estrucutra Spring boot

---
+++ src/main/java/event/eventFilmsPro/EventFilmsProApplication.java
@@ -0,0 +1,13 @@
+package event.eventFilmsPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventFilmsProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventFilmsProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/event/eventFilmsPro/EventFilmsProApplicationTests.java
@@ -0,0 +1,13 @@
+package event.eventFilmsPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventFilmsProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
